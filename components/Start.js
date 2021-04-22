c19b7d0d024fffea4350bd8ded678b7e7e30bddf
Zenia Gist <zeniagist@gmail.com>
2021-04-21 22:27:19 -0500

    styled start home screen

--- components/Start.js
+++ components/Start.js
@@ -1,59 +1,206 @@
 import React from 'react';
-import { StyleSheet, View, Text, TextInput, Button, Alert, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button, ImageBackground } from 'react-native';
 
 export default class Start extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: '' };
-  }
-
-  // alert the user input
-  alertMyText(input = []) {
-    Alert.alert(input.text);
+    this.state = {
+      name: '',
+      bgColor: '',
+    };
   }
 
   render() {
+    // import image for Start background
+    const image = require("../assets/startBackgroundimage.png");
+
     return (
-      <View style={styles.container}>
-        <Text>Hello Screen1!</Text>
+      <ImageBackground source={image} style={styles.backgroundImage} >
+        <View style={styles.container} >
 
-        {/* button go to Screen 2 */}
-        <Button
-          title="Go to Chat"
-          onPress={() => this.props.navigation.navigate('Chat')}
-        />
+          {/* app title */}
+          <View style={styles.appTitleContainer}>
+            <Text style={styles.appTitle}>Chatterbox</Text>
+          </View>
 
-        {/* text input */}
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={(text) => this.setState({ text })}
-          value={this.state.text}
-          placeholder='Type here ...'
-        />
-        {/* button alert with text from text input */}
-        <Button
-          onPress={() => {
-            this.alertMyText({ text: this.state.text });
-          }}
-          title="Press Me"
-        />
-        {/* copies what was written in text input */}
-        <Text>You wrote: {this.state.text}</Text>
+          <View style={styles.userContainer} >
+            {/* username input */}
+            <TextInput
+              style={styles.textInput}
+              onChangeText={(name) => this.setState({ name })}
+              value={this.state.name}
+              placeholder='Type username here'
+            />
+            <Text style={styles.usernameText}>{this.state.name} </Text>
 
-        {/* scrollview */}
-        <ScrollView>
-          <Text style={{ fontSize: 110 }}>This text is so big! And so long! You have to scroll!</Text>
-        </ScrollView>
-      </View>
+            {/* choose background color */}
+            <View style={styles.colorContainer} >
+
+              <Text style={styles.colorTitle} >
+                Choose Background Color:
+              </Text>
+              {/* background color options */}
+              <View style={styles.colorOptions} >
+
+                <View
+                  onPress={() => this.setState({ bgColor: '#090C08' })}
+                  style={styles.colorOne}
+                ></View>
+
+                <View
+                  onPress={() => this.setState({ bgColor: '#474056' })}
+                  style={styles.colorTwo}
+                ></View>
+
+                <View
+                  onPress={() => this.setState({ bgColor: '#8A95A5' })}
+                  style={styles.colorThree}
+                ></View>
+
+                <View
+                  onPress={() => this.setState({ bgColor: '#B9C6AE' })}
+                  style={styles.colorFour}
+                ></View>
+              </View>
+            </View>
+
+            {/* navigate to chat screen */}
+            <Button
+              color='#757083'
+              title="Start Chatting"
+              onPress={() => this.props.navigation.navigate('Chat',
+                {
+                  name: this.state.name,
+                  bgColor: this.state.bgColor
+                }
+              )}
+            />
+          </View>
+
+        </View>
+      </ImageBackground>
     )
   }
 }
 
-// stylesheet
+//---------- Styles ----------//
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+  },
+
   container: {
     flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center'
-  }
+    padding: 15,
+  },
+
+  appTitleContainer: {
+    width: '88%',
+    height: '56%'
+  },
+
+  appTitle: {
+    position: "relative",
+    textAlign: "center",
+    margin: 20,
+    marginRight: "auto",
+    marginLeft: "auto",
+    color: "#FFFFFF",
+    top: 100,
+    height: "44%",
+    fontSize: 45,
+    fontWeight: "600",
+  },
+
+  userContainer: {
+    width: "88%",
+    height: '44%',
+    backgroundColor: "white",
+    padding: 'auto',
+    borderRadius: 5,
+  },
+
+  textInput: {
+    borderColor: 'gray',
+    color: '#757083',
+    borderWidth: 1,
+    fontSize: 16,
+    fontWeight: '300',
+    opacity: 50,
+    position: 'relative',
+    padding: 5,
+    margin: 20,
+  },
+
+  usernameText: {
+    fontSize: 16,
+    fontWeight: '600',
+    marginLeft: 20,
+  },
+
+  colorContainer: {
+    margin: 20
+  },
+
+  colorTitle: {
+    fontSize: 16,
+    fontWeight: '300',
+    color: '#757083',
+    opacity: 100
+  },
+
+  colorOptions: {
+    flexDirection: 'row',
+    marginRight: 5,
+    position: 'relative'
+  },
+
+  colorOne: {
+    backgroundColor: '#090C08',
+    width: 50,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    margin: 10
+  },
+
+  colorTwo: {
+    backgroundColor: '#474056',
+    width: 50,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    margin: 10
+  },
+
+  colorThree: {
+    backgroundColor: '#8A95A5',
+    width: 50,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    margin: 10
+  },
+
+  colorFour: {
+    backgroundColor: '#B9C6AE',
+    width: 50,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    margin: 10
+  },
+
+  chatButton: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: 'red'
+  },
+
 });
